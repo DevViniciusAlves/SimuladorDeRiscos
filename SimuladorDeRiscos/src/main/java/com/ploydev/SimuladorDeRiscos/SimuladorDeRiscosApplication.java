889f0872e8a02130798c41575e2effdc5f788b47
vinic <viniciushf0360@gmail.com>
2026-01-25 11:50:28 -0300

    first commit

---
+++ SimuladorDeRiscos/src/main/java/com/ploydev/SimuladorDeRiscos/SimuladorDeRiscosApplication.java
@@ -0,0 +1,13 @@
+package com.ploydev.SimuladorDeRiscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDeRiscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorDeRiscosApplication.class, args);
+	}
+
+}
---
+++ SimuladorDeRiscos/src/test/java/com/ploydev/SimuladorDeRiscos/SimuladorDeRiscosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ploydev.SimuladorDeRiscos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDeRiscosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
